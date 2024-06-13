2efeb447228a2fc8dbcef911d0a24209f5529667
dimasyoga42 <dimasyoga9090@gmail.com>
2024-06-13 17:52:33 +0700

    add:modul cross

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express"
+import cors from "cors";
 import bodyParser from "body-parser";
 import router from "./routes/apiRouter.js"
 
