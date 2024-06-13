1f9bb9160bcef5d201ad0c3600993b2344735e72
dimasyoga42 <dimasyoga9090@gmail.com>
2024-06-13 18:58:04 +0700

    refactor: add title property to transformed data from Supabase

--- controller/function.js
+++ controller/function.js
@@ -6,13 +6,27 @@ let supabaseKey = process.env.SUPABASE_KEY
 let supabaseUrl = process.env.SUPABASE_URL
 const supabase = createClient(supabaseUrl, supabaseKey)
 //controller postData
-export const getData = async (req, res, next) => {
-  const db = await supabase.from('aadb').select()
-  res.json({
-    db
-  })
-  console.log({db})
-}
+export const getData = async (req, res, next) => {try {
+    // Mengambil data dari tabel Supabase
+    const { data, error, status, statusText } = await supabase
+      .from('aadb')
+      .select('arab, nama, arti');
+
+    if (error) throw error;
+    const transformedData = "Asmaul husnah"
+
+    // Mengirim respons JSON dengan data yang telah dimodifikasi
+    res.status(status).json({
+      title: transformedData,
+      status,
+      count: data.length,
+      data,
+      statusText
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching data', error });
+  }
+};
 export const postData = async (req, res, next) => {
   const { nama, arab, arti } = req.body
   const db = await supabase.from('aadb').insert({ nama, arab, arti });
